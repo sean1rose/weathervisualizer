4387f3a18031a2f145af2418cc7dddbd1297c5a4
Sean Rose <sean1rose@gmail.com>
2016-08-30 23:36:14 -0700

    Still doesn't work but working on app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
+import '../node_modules/react-vis/main.css';
+import {json} from 'd3-request';
+import * as CONSTANTS from './constants';
+// import secrets from './secrets';
+// const {API} = secrets;
+
+import LargeChart from './large-chart';
+import SmallChart from './small-chart';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-        </p>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      highlighted: null
+    };
+    this.highlightX = this.highlightX.bind(this);
+    this.processResults = this.processResults.bind(this);
+  }
+
+  componentWillMount() {
+    // d3-request json function
+    json(`${CONSTANTS.QUERY_PREFIX}?=${CONSTANTS.CITY_ID}&appid=${CONSTANTS.API}&units=imperial`, this.processResults);
+  }
+
+  highlightX(highlighted) {
+    this.setState({highlighted});
+  }
+
+  processResults(error, queryResults) {
+    if (error) {
+      this.setState({error});
+    }
+    const data = CONSTANTS.KEYS.map(key => ({
+      key,
+      values: queryResults.list.map((d, i) => ({
+        i,
+        x: d.dt * 1000,
+        y: d[key.key1] ? d[key.key1][key.key2] || 0 : 0
+      }))
+    })).reduce((prev, cur) => {
+      return {...prev, [cur.key.name]: cur.values}
+    }, {
+      'city-name': (
+        queryResults &&
+        queryResults.city &&
+        queryResults.city.name
+      ) || 'Unknown'
+    });
+    this.setState({data});
   }
 }
-
-export default App;
---
+++ src/constants.js
@@ -0,0 +1,40 @@
+export const CITY_ID = 5368361;
+export const QUERY_PREFIX = 'http://api.openweathermap.org/data/2.5.forecast';
+export const API = '7810e14ab82bf0938dec6f64a9991cea';
+export const KEYS = [
+	{
+		name: 'Temperature',
+		key1: 'main',
+		key2: 'temp'
+	},
+	{
+		name: 'Min Temperature',
+		key1: 'main',
+		key2: 'temp_min'
+	},
+	{
+		name: 'Max Temperature',
+		key1: 'main',
+		key2: 'temp_max'
+	},
+	{
+		name: 'Pressure',
+		key1: 'main',
+		key2: 'pressure'
+	},
+	{
+		name: 'Cloudiness',
+		key1: 'clouds',
+		key2: 'all'
+	},
+	{
+		name: 'Wind speed',
+		key1: 'wind',
+		key2: 'speed'
+	},
+	{
+		name: 'Rain',
+		key1: 'rain',
+		key2: '3h'
+	}
+]
